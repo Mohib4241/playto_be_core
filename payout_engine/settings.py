50ca51044dbd3587f97591e258913f0f235fc2cf
Mohibbul Bari <147559471+Mohib4241@users.noreply.github.com>
2026-04-27 08:03:25 +0530

    fix: set CONN_MAX_AGE to 0 for better stability with Neon connection pooler

--- payout_engine/settings.py
+++ payout_engine/settings.py
@@ -65,7 +65,7 @@ if not DATABASE_URL:
 DATABASES = {
     "default": dj_database_url.config(
         default=DATABASE_URL,
-        conn_max_age=600,
+        conn_max_age=0,
         ssl_require=True
     )
 }
