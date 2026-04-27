3ccca0fc3f807086a0fb000b052e14e2ed574a73
Mohibbul Bari <147559471+Mohib4241@users.noreply.github.com>
2026-04-27 07:40:46 +0530

    chore: remove migration step from start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-# 1. Run migrations
-echo "Running migrations..."
-python manage.py migrate --noinput
-
-# 2. Start Celery worker in the background
+# 1. Start Celery worker in the background
 # We use --concurrency=1 to save memory
 echo "Starting Celery Worker..."
 celery -A payout_engine worker --loglevel=info -Q payouts,celery --concurrency=1 &
 
-# 3. Start Django development server (Lightest memory usage)
+# 2. Start Django development server (Lightest memory usage)
 echo "Starting Django Server..."
 python manage.py runserver 0.0.0.0:$PORT
